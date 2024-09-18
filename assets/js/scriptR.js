58482d205be2d8420799242d0e307c9ed88b4e29
painisreal <ariknesh05@.com>
2024-09-18 12:10:24 +0530

    feat:data

---
+++ assets/js/scriptR.js
@@ -0,0 +1,24 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxqMJET3sPMdoM9i2WP4A9nOOEu8PjJlENFNfmqqIt-P-kx-bDFASkVcl6IHzAqUc2qrw/exec'
+const form = document.forms['submit-to-google-sheet-acc']
+const msgt = document.getElementById('msgt');
+const msg = document.getElementById('msg');
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent."
+        setTimeout(function(){
+                msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+    })
+    
+    if (msg.innerHTML == "") {
+      function time(){
+        msgt.innerHTML = "Sending....."
+        setTimeout(function(){
+                msgt.innerHTML = ""
+        },3000)
+    }}
